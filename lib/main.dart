c365c7cd58e385ae9eaff5575fda22ccc7aa6f0e
nexzonerival6 <nexzonerival6@gmail.com>
2024-10-04 03:06:11 +0000

    modified:   lib/main.dart 	modified:   lib/start_screen.dart 	modified:   pubspec.yaml

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:myapp/start_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(body: StartScreen(),
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 126, 71, 221),
+            Color.fromARGB(255, 107, 15, 168),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight
+          ),
+        ),
+        child: const StartScreen(),
+        ),
       ),
     ),
   ); //runApp
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,6 +5,16 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return const Text('Start Screen');
+    return Center(
+      child: Column(
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+            
+        ],),
+      );
   }
 }
