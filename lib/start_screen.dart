1d1033d64b0d82664713ad2f69d9cacececcc7c6
nexzonerival6 <nexzonerival6@gmail.com>
2024-10-04 03:37:41 +0000

    new file:   .vscode/settings.json 	modified:   lib/start_screen.dart 	modified:   pubspec.lock 	modified:   pubspec.yaml 	deleted:    test/widget_test.dart

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -4,17 +4,34 @@ class StartScreen extends StatelessWidget {
   const StartScreen({super.key});
 
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return Center(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.center, // Added for centering
         children: [
           Image.asset(
             'assets/images/quiz-logo.png',
             width: 300,
             color: const Color.fromARGB(150, 255, 255, 255),
           ),
-            
-        ],),
-      );
+          const SizedBox(height: 60),
+          const Text(
+            'Learn Flutter the Fun Way', 
+            style: TextStyle(
+              color: Color.fromARGB(255, 245, 244, 247),
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(height: 30),  // The missing comma was here
+          OutlinedButton(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+            ), 
+            child: const Text('Start Quiz'),
+          ),
+        ],
+      ),
+    );
   }
 }
